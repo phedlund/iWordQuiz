059a09e37f90215199fc04e056352f8f64ce3739
Peter Hedlund <peter@peterandlinda.com>
2013-12-30 11:21:17 -0800

    Dynamic Type for iPhone multiple choice.

--- iWordQuiz/UIFont+WQDynamic.m
+++ iWordQuiz/UIFont+WQDynamic.m
@@ -16,24 +16,20 @@
 	NSString *contentSize = [UIApplication sharedApplication].preferredContentSizeCategory;
 	
 	if ([contentSize isEqualToString:UIContentSizeCategoryExtraSmall]) {
-		fontSize = 13.0;
+		fontSize = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)? 11.0f : 13.0;
 	} else if ([contentSize isEqualToString:UIContentSizeCategorySmall]) {
-		fontSize = 15.0;
+		fontSize = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)? 12.0f : 15.0;
 	} else if ([contentSize isEqualToString:UIContentSizeCategoryMedium]) {
-		fontSize = 17.0;
+		fontSize = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)? 13.0f : 17.0;
 	} else if ([contentSize isEqualToString:UIContentSizeCategoryLarge]) {
-		fontSize = 19.0;
+		fontSize = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)? 14.0f : 19.0;
 	} else if ([contentSize isEqualToString:UIContentSizeCategoryExtraLarge]) {
-		fontSize = 21.0;
+		fontSize = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)? 15.0f : 21.0;
 	} else if ([contentSize isEqualToString:UIContentSizeCategoryExtraExtraLarge]) {
-		fontSize = 23.0;
+		fontSize = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)? 16.0f : 23.0;
 	} else if ([contentSize isEqualToString:UIContentSizeCategoryExtraExtraExtraLarge]) {
-		fontSize = 25.0;
+		fontSize = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)? 17.0f : 25.0;
 	}
-    
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
-        fontSize = fontSize - 3;
-    }
 	
 	if ([textStyle isEqualToString:UIFontTextStyleHeadline] || [textStyle isEqualToString:UIFontTextStyleSubheadline]) {
 		return [UIFont boldSystemFontOfSize:fontSize + 1];
